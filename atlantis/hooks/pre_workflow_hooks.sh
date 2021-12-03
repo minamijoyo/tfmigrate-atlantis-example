4ca7d9aecef9bc817c675e38ad2089a6cbd678e1
Masayuki Morita <minamijoyo@gmail.com>
2021-12-03 16:36:16 +0900

    Fix comments for atlantis settings

--- atlantis/hooks/pre_workflow_hooks.sh
+++ atlantis/hooks/pre_workflow_hooks.sh
@@ -6,9 +6,10 @@ set -euo pipefail
 echo "start pre_workflow_hooks"
 
 # Check if there are any unapplied migrations left.
-# This example assumes history mode. If you are not using history mode,
-# you need to detect a new migration file from pull request metadata by yourself.
+# This example assumes history mode.
 # https://github.com/minamijoyo/tfmigrate#history-block
+# If you are not using history mode, you need to detect a new migration file
+# from pull request metadata by yourself.
 MIGRATION_FILES=$(tfmigrate list --status=unapplied)
 echo "MIGRATION_FILES: $MIGRATION_FILES"
 
